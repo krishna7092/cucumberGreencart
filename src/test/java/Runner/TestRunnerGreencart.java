8cbc5616260da5ef487f2efc87047d035349b0b1
--krishna <krishnakumar7092@gmail.com>
2023-12-15 22:34:07 +0530

    hooks and extendreports

--- src/test/java/Runner/TestRunnerGreencart.java
+++ src/test/java/Runner/TestRunnerGreencart.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "C:\\Users\\krishna\\cucumberfullframework\\Rahulshetty\\src\\test\\java\\Featuresfile",
 glue = "Stepdefinitions",
 dryRun = false,monochrome = true ,
-plugin = {"pretty", "html:\\Reports\\result.html"},
+plugin = {"pretty", "html:\\Reports\\result.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 tags="@greencart")
 
 public class TestRunnerGreencart extends AbstractTestNGCucumberTests{
--- src/test/java/Stepdefinitions/hooks.java
+++ src/test/java/Stepdefinitions/hooks.java
@@ -1,17 +1,23 @@
 package Stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+
+import Utils.InjectUtils;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 
 public class hooks {
-	    @Before
-		public void openchrome() {
-	    	
-		}
+	public WebDriver driver;
+	public hooks(InjectUtils utils) {
+		// TODO Auto-generated constructor stub
+		driver=utils.driver;
+	}
+	
 	    
 	    @After
 	    public void close() {
+	    	driver.close();
 	    	
-	    	System.out.println("close the browser");
 	    }
 }
